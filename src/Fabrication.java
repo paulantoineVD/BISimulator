577e08dd42345ba2417d7582aa05c2b4d747be1d
Nikouda <30809465+Nikouda@users.noreply.github.com>
2018-07-02 10:26:02 +0200

    Creation class fabrication + constructeur

---
+++ src/Fabrication.java
@@ -0,0 +1,12 @@
+
+public class Fabrication extends Machine{
+	private String[] variante;
+	
+	public Fabrication() {
+		super();
+		this.variante = new String[0];
+	}
+	
+	public Fabrication(String[] variante,  )
+	
+}
--- src/Machine.java
+++ src/Machine.java
@@ -1,4 +1,37 @@
 
 public class Machine {
+	protected int id;
+	protected int cadence;
+	protected int changement;
+	
+	public int getCadence() {
+		return cadence;
+	}
 
+	public void setCadence(int cadence) {
+		this.cadence = cadence;
+	}
+
+	public int getChangement() {
+		return changement;
+	}
+
+	public void setChangement(int changement) {
+		this.changement = changement;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Machine() {
+		this.id = 0;
+		this.cadence = 0;
+		this.changement = 0;
+	}
+	public Machine(int cadence, int changement) {
+		this.cadence = cadence;
+		this.changement = changement;
+	}
+	
 }
