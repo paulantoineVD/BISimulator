23d210eb9211c7abde3c6bb745a9a97a20225407
Nikouda <30809465+Nikouda@users.noreply.github.com>
2018-07-02 10:35:15 +0200

    class Conditionnement constructeur + getter/setter

--- src/Conditionnement.java
+++ src/Conditionnement.java
@@ -1,4 +1,17 @@
 
-public class Conditionnement {
+public class Conditionnement extends Machine{
+	
+	private String packaging;
+	
+	public Conditionnement() {
+		super();
+		this.packaging = "";
+	}
+	
+	public Conditionnement(String packaging, int cadence, int changement) {
+		super(cadence, changement);
+		this.packaging = packaging;
+	}
+	
 
 }
--- src/Fabrication.java
+++ src/Fabrication.java
@@ -2,11 +2,22 @@
 public class Fabrication extends Machine{
 	private String[] variante;
 	
+	public String[] getVariante() {
+		return variante;
+	}
+
+	public void setVariante(String[] variante) {
+		this.variante = variante;
+	}
+
 	public Fabrication() {
 		super();
 		this.variante = new String[0];
 	}
 	
-	public Fabrication(String[] variante,  )
+	public Fabrication(String[] variante, int cadence, int changement) {
+		super(cadence, changement);
+		this.variante = variante;
+	}
 	
 }
