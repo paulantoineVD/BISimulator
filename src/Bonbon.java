58ea81d06756612edeba30f95b611936ed82935d
Nikouda <30809465+Nikouda@users.noreply.github.com>
2018-07-03 13:06:46 +0200

    class bonbon + class stock

---
+++ src/Bonbon.java
@@ -0,0 +1,42 @@
+
+public class Bonbon {
+	
+	private String Bnom;
+	private String Bcouleur;
+	private String Bvariante;
+	private String Btexture;
+	private String BConditionnement;
+	
+	public String getBnom() {
+		return Bnom;
+	}
+	public void setBnom(String bnom) {
+		Bnom = bnom;
+	}
+	public String getBcouleur() {
+		return Bcouleur;
+	}
+	public void setBcouleur(String bcouleur) {
+		Bcouleur = bcouleur;
+	}
+	public String getBvariante() {
+		return Bvariante;
+	}
+	public void setBvariante(String bvariante) {
+		Bvariante = bvariante;
+	}
+	public String getBtexture() {
+		return Btexture;
+	}
+	public void setBtexture(String btexture) {
+		Btexture = btexture;
+	}
+	public String getBConditionnement() {
+		return BConditionnement;
+	}
+	public void setBConditionnement(String bConditionnement) {
+		BConditionnement = bConditionnement;
+	}
+	
+
+}
